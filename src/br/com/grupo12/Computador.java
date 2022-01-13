58e2d0ba66184ef80f118cd27ec44295c8921979
jaovmorais <joao.morais00@hotmail.com>
2022-01-12 23:05:31 -0300

    Update Computador.java

--- src/br/com/grupo12/Computador.java
+++ src/br/com/grupo12/Computador.java
@@ -15,8 +15,6 @@ public class Computador extends Jogador{
 
             int linhaNavioComputer;
             int colunaNavioComputer;
-//            int[] validaLinha = new int[3];
-//            int[] validaColuna = new int[3];
             Random sorteio = new Random();
 
             for (int i = 0; i < this.quantidadeNavios; i++) {
@@ -30,23 +28,6 @@ public class Computador extends Jogador{
 
                 this.tabuleiro.coordenadasTabuleiro[linhaNavioComputer][colunaNavioComputer] = Legendas.NAVIO_POSICIONADO.getLegenda();
 
-//                validaLinha[i] = linhaNavioComputer;
-//                validaColuna[i] = colunaNavioComputer;
-            /*System.out.println(linhaNavioComputer);
-            System.out.println(colunaNavioComputer);*/
-
-//                for (int anterior = 0; anterior < i; anterior++) {
-//
-//                    boolean b = (validaLinha[i] == validaLinha[anterior]) && (validaColuna[i] == validaColuna[anterior]);
-//                    if (b) {
-//                        linhaNavioComputer = sorteio.nextInt(10);
-//                        colunaNavioComputer = sorteio.nextInt(10);
-//                        /*System.out.println(linhaNavioComputer);
-//                        System.out.println(colunaNavioComputer);*/
-//                        this.tabuleiro.coordenadasTabuleiro[linhaNavioComputer][colunaNavioComputer] = Legendas.NAVIO_POSICIONADO.getLegenda();
-//
-//                    }
-//                }
             }
         }
 
